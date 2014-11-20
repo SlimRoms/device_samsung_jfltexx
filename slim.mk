ef96f02a437a69e3f6e7df9452641e4ded8f6468
dcd <dcd1182@gmail.com>
2014-11-19 18:55:56 -0600

    jfltexx: update props, deps, blobs, de-unify
    Change-Id: I69da06228643d8154400bb25489870e7a5b59279
    
    Conflicts:
    	BoardConfig.mk
    	cm.dependencies
    	cm.mk
    	full_jfltexx.mk
    	system.prop

--- slim.mk
+++ slim.mk
@@ -1,22 +1,18 @@
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := jfltexx
-
-# Inherit some common SLIM stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltexx \
+  TARGET_DEVICE=jfltexx \
+  BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
 
 PRODUCT_NAME := slim_jfltexx
 PRODUCT_DEVICE := jfltexx
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9505
-PRODUCT_MANUFACTURER := Samsung
+
 
